9c12138eb4df222205d9fd800204117b2a3c34da
Paweł Dróżdż <paweldrozdz94@gmail.com>
2018-11-03 18:37:00 +0100

    AddUserRestaurantAndAdressModels

---
+++ YourPlace/Models/Adress.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace YourPlace.Models
+{
+    public class Adress
+    {
+        public int Id { get; set; }
+        public string City { get; set; }
+        public string NameOfTheStreet { get; set; }
+        public int BuildingNumber { get; set; }
+    }
+}
