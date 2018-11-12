0f0b88c411eaa77317943e3060d6d2a65170d10a
Paweł Dróżdż <paweldrozdz94@gmail.com>
2018-11-12 21:19:36 +0100

    Add CustomersController and NewCustomer form

---
+++ YourPlace/Controllers/CustomersController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using YourPlace.Models;
+using YourPlace.ViewModels;
+
+namespace YourPlace.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+        
+        public ActionResult Index()
+        {
+            var customers = new AllCustomersViewModel()
+            {
+                Customers = _context.Customers.ToList()
+            };
+            return View(customers);
+        }
+
+        public ActionResult NewCustomer()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public ActionResult Save(Customer customer)
+        {
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Customers");
+        }
+
+        
+    }
+}
---
+++ YourPlace/ViewModels/AllCustomersViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using YourPlace.Models;
+
+namespace YourPlace.ViewModels
+{
+    public class AllCustomersViewModel
+    {
+        public List<Customer> Customers { get; set; }
+    }
+}
