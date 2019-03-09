487d4e79c6db9e5a313f1ae9a0cd8ac283b2fe6a
Paweł Dróżdż <paweldrozdz94@gmail.com>
2019-03-09 23:43:00 +0100

    Time of posting for comments

--- YourPlace/Models/Comment.cs
+++ YourPlace/Models/Comment.cs
@@ -14,5 +14,40 @@ namespace YourPlace.Models
 
         public int RestaurantId { get; set; }
         public Restaurant Restaurant { get; set; }
+
+        //Refactor!!!
+        public Time TimeSinceAdding(DateTime createDate)
+        {
+            var now = DateTime.Now;
+            TimeSpan interval = now - createDate;
+
+            if (interval.TotalSeconds < 1)
+            {
+                return new Time { Number = 1, TimeUnit = "sec." };
+            }
+            else if (interval.TotalSeconds < 60)
+            {
+                return new Time { Number = Math.Round(interval.TotalSeconds), TimeUnit = "sec."};
+            }
+            else if (interval.TotalMinutes < 60)
+            {
+                return new Time { Number = Math.Round(interval.TotalMinutes), TimeUnit = "min." };
+            }
+            else if (interval.TotalHours < 24)
+            {
+                return new Time { Number = Math.Round(interval.TotalHours), TimeUnit = "h." };
+            }
+            else
+            {
+                return new Time { Number = Math.Round(interval.TotalDays), TimeUnit = "d." };
+            }
+        }
+
+        //Refactor!!!
+        public class Time
+        {
+            public double Number { get; set; }
+            public string TimeUnit { get; set; }
+        }
     }
 }
--- YourPlace/Models/Reply.cs
+++ YourPlace/Models/Reply.cs
@@ -17,5 +17,40 @@ namespace YourPlace.Models
 
         public int CommentId { get; set; }
         public Comment Comment { get; set; }
+
+        //Refactor!!!
+        public Time TimeSinceAdding(DateTime createDate)
+        {
+            var now = DateTime.Now;
+            TimeSpan interval = now - createDate;
+
+            if (interval.TotalSeconds < 1)
+            {
+                return new Time { Number = 1, TimeUnit = "sec." };
+            }
+            else if (interval.TotalSeconds < 60)
+            {
+                return new Time { Number = Math.Round(interval.TotalSeconds), TimeUnit = "sec." };
+            }
+            else if (interval.TotalMinutes < 60)
+            {
+                return new Time { Number = Math.Round(interval.TotalMinutes), TimeUnit = "min." };
+            }
+            else if (interval.TotalHours < 24)
+            {
+                return new Time { Number = Math.Round(interval.TotalHours), TimeUnit = "h." };
+            }
+            else
+            {
+                return new Time { Number = Math.Round(interval.TotalDays), TimeUnit = "d." };
+            }
+        }
+
+        //Refactor!!!
+        public class Time
+        {
+            public double Number { get; set; }
+            public string TimeUnit { get; set; }
+        }
     }
 }
